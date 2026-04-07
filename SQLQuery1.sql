c5fbb0ed7f1b7ea3f441a12bfdd365ea0b08a7b8
senatino2000 <senatino>
2026-04-07 11:30:24 -0500

    Mensaje en el Dashboard

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -35,54 +35,6 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE sp_verUsuarios
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT * FROM Usuarios;
-END
-GO
-
-CREATE PROCEDURE sp_verUsuario
-    @id INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-    SELECT id, nombre, apellido, dni, email 
-    FROM Usuarios 
-    WHERE id = @id;
-END
-GO
-
-CREATE PROCEDURE sp_EditarUser
-    @id INT,
-    @nombre NVARCHAR(50),
-    @apellido NVARCHAR(50),
-    @dni VARCHAR(20),
-    @email NVARCHAR(100),
-    @contrasenia NVARCHAR(255)
-AS
-BEGIN
-    SET NOCOUNT ON;
-    UPDATE Usuarios
-    SET nombre = @nombre,
-        apellido = @apellido,
-        dni = @dni,
-        email = @email,
-        contrasenia = @contrasenia
-    WHERE id = @id;
-END
-GO
-
-CREATE PROCEDURE sp_EliminarUser
-    @id INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-    DELETE FROM Usuarios WHERE id = @id;
-END
-GO
-
 CREATE PROCEDURE sp_LoginUser
     @email NVARCHAR(100),
     @contrasenia NVARCHAR(255)
--- WebLoginMVC/Controllers/UsuarioController.cs
+++ WebLoginMVC/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using WebLoginMVC.Controllers.dao;
 using WebLoginMVC.Models;
 
